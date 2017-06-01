f4a8e4bc7206c1809d3ce689bafb7b38d3258793
Jianming Zhou <jz1584@nyu>
2017-06-01 10:06:45 -0400

    changed

--- Map.R
+++ Map.R
@@ -17,17 +17,30 @@ college<-dget('cleaned data/colleges/college')
 
 
 callCenters<-dget('cleaned data/callCenters')
-callCenters$Cost<-NA
-callCenters[is.na(callCenters$annualCost.Max),]$Cost<-paste('$',callCenters[is.na(callCenters$annualCost.Max),]$annual.cost.per.sf)
-callCenters[!is.na(callCenters$annualCost.Max),]$Cost<-paste('$',callCenters[!is.na(callCenters$annualCost.Max),]$annual.cost.per.sf,'-','$',callCenters[!is.na(callCenters$annualCost.Max),]$annualCost.Max)
-callCenters$MeanCost<-rowMeans(callCenters[,c('annual.cost.per.sf','annualCost.Max')],na.rm = TRUE)
 
 
 
+#distance labeling
+netdist<-callCenters
+netdist$ID<-1:nrow(netdist)
+Wynn<-geocode('3131 S Las Vegas Blvd, Las Vegas, NV 89109')
+netdist$name<-as.character(netdist$name)
+#netdist[(nrow(netdist)+1):(nrow(netdist)*2),]<-NA #create empty rows
+netdist[(nrow(netdist)+1):(nrow(netdist)*2),]$name<-'Wynn Hotel'
+netdist[13:24,c("lon","lat")]<-Wynn
+netdist[13:24,c('ID')]<-1:12
 
 
-
-
+callCenters$Wynn.distance.Mins<-NA
+callCenters$Wynn.distance.mile<-NA
+for (i in 1:nrow(callCenters)){
+  temp<-mapdist(as.numeric(Wynn),as.numeric(callCenters[i,c("lon","lat")]),mode = 'driving')
+  callCenters$Wynn.distance.Mins[i]<-round(temp$minutes,2)
+  callCenters$Wynn.distance.mile[i]<-round(temp$miles,2)
+}
+  
+  
+  
 
 #Map block color Setting
 clrs <- RColorBrewer::brewer.pal(5, "YlOrRd")
@@ -54,6 +67,11 @@ local1<-makeIcon(
   iconWidth = 30, iconHeight = 30)
 
 
+school<-makeIcon(
+  iconUrl = 'logo/graduation_cap.png',
+  iconWidth = 40, iconHeight = 30)
+
+
 #popup magines:
 
 callcenterImages<-c("<img src='https://raw.githubusercontent.com/jz1584/Map/master/CityCenterWest.PNG' style='width:337px;height:150px;'>",
@@ -75,12 +93,12 @@ callcenterImages<-c("<img src='https://raw.githubusercontent.com/jz1584/Map/mast
 
 
 
-
-
+##################################################################################################################################################################
+##################################################################################################################################################################
 # Mapping Part:
-leaflet()%>% addTiles()%>% #addProviderTiles("CartoDB.Positron")%>%
+LasVegas<-leaflet()%>% addTiles()%>% #addProviderTiles("CartoDB.Positron")%>%
   fitBounds(lat1=36.337530 ,lng1=-115.390434,lat2=36.097106,lng2=-114.980507)%>%
-  addPolylines(data=vegas,weight=2,color='blue',opacity=1,group = '<font color="#1E43A8" size=4><u>Las Vegas City</u></font>')%>%
+  addPolylines(data=vegas,weight=2,color='black',opacity=1,group = '<font color="#000000" size=4><u>Las Vegas City</u></font>')%>%
   
   # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  locations of interest 
   addMarkers(data=callCenters,lng = callCenters$lon,lat=callCenters$lat,
@@ -93,7 +111,8 @@ leaflet()%>% addTiles()%>% #addProviderTiles("CartoDB.Positron")%>%
              ),
              group = '<font color="#1E43A8" size=4><u>Possible Call Center Locations</u></font>')%>%
   
-  
+  addPulseMarkers(lng=Wynn$lon,lat=Wynn$lat,icon=makePulseIcon(heartbeat = 0.8,iconSize = 40),
+                  group ='<font color="#cc0000" size=4><u>Wynn Hotel</u></font> <b>(Red-Pulse Markers) with driving distance to call centers</b>')%>%
   # addPopups(
   #   data=callCenters,lng = callCenters$lon,lat=callCenters$lat,popup=paste0(
   #     'Location Name:',callCenters$name,"<br>",
@@ -116,7 +135,7 @@ addPolygons(data=vegas,fillOpacity = 0.5,stroke=FALSE,group = '<font color="#cc0
               "General Population: ",vegas$Population,'<br>',
               "<b>Labor Population Density: ",round(vegas$Labor_SQMI,0)," per SQMI</b>"
             )) %>%
-  addMarkers(lng=college$lon,lat=college$lat,popup = college$Name,group = '<font color="#cc0000" size=4><u>Labor Market Demographics </u></font>(<b>COLLEGE</b>)'
+  addMarkers(lng=college$lon,lat=college$lat,icon = school,popup = college$Name,group = '<font color="#cc0000" size=4><u>Labor Market Demographics </u></font>(<b>COLLEGE</b>)'
   )%>%
   
   addPolygons(data=vegas,fillOpacity = 0.5,stroke=FALSE,group = '<font color="#08519C" size=4><u>Local Household Income</u></font>',
@@ -129,11 +148,11 @@ addPolygons(data=vegas,fillOpacity = 0.5,stroke=FALSE,group = '<font color="#cc0
                 "Labor Population Density: ",round(vegas$Labor_SQMI,0)," per SQMI"
               )) %>%
   
-  addPopups(lng=college[college$Size500More=='YES',]$lon,lat=college[college$Size500More=='YES',]$lat,
-            popup =paste('<font color="#cc0000"size=2><b>',college[college$Size500More=='YES',]$Name,'</b></u></font>'),
-            options = popupOptions(closeButton = TRUE),
-            group = '<font color="#cc0000" size=4><u>Labor Market Demographics </u></font>(<b>COLLEGE</b>)'
-  )%>%
+  # addPopups(lng=college[college$Size500More=='YES',]$lon,lat=college[college$Size500More=='YES',]$lat,
+  #           popup =paste('<font color="#cc0000"size=2><b>',college[college$Size500More=='YES',]$Name,'</b></u></font>'),
+  #           options = popupOptions(closeButton = TRUE),
+  #           group = '<font color="#cc0000" size=4><u>Labor Market Demographics </u></font>(<b>COLLEGE</b>)'
+  # )%>%
   
   
   addMeasure(position = 'bottomright',completedColor = "#000000",activeColor = '#FF0000')%>%
@@ -141,14 +160,16 @@ addPolygons(data=vegas,fillOpacity = 0.5,stroke=FALSE,group = '<font color="#cc0
   hideGroup(c(
     '<font color="#08519C" size=4><u>Local Household Income</u></font>',
     '<font color="#cc0000" size=4><u>Labor Market Demographics </u></font>(<b>COLLEGE</b>)',
-    '<font color="#000000" size=4><u>Las Vegas Strip</u></font>'
+    '<font color="#000000" size=4><u>Las Vegas Strip</u></font>',
+    '<font color="#cc0000" size=4><u>Wynn Hotel</u></font> <b>(Red-Pulse Markers) with driving distance to call centers</b>'
   ))%>%
-  addLayersControl(overlayGroups =  c('<font color="#1E43A8" size=4><u>Las Vegas City</u></font>',
+  addLayersControl(overlayGroups =  c('<font color="#000000" size=4><u>Las Vegas City</u></font>',
                                       '<font color="#1E43A8" size=4><u>Possible Call Center Locations</u></font>',
                                       '<font color="#08519C" size=4><u>Local Household Income</u></font>',
                                       '<font color="#cc0000" size=4><u>Labor Market Demographics </u></font>(<b>Population Density,age 18-62</b>)',
                                       '<font color="#cc0000" size=4><u>Labor Market Demographics </u></font>(<b>COLLEGE</b>)',
-                                      '<font color="#000000" size=4><u>Las Vegas Strip</u></font>'
+                                      '<font color="#000000" size=4><u>Las Vegas Strip</u></font>',
+                                      '<font color="#cc0000" size=4><u>Wynn Hotel</u></font> <b>(Red-Pulse Markers) with driving distance to call centers</b>'
                                       
                                       #'<font color="#008000" size=2><u>NYC Agencies Office Location</u></font>',
   ),
@@ -161,9 +182,6 @@ addPolygons(data=vegas,fillOpacity = 0.5,stroke=FALSE,group = '<font color="#cc0
             title = HTML('<font color="#FF3371" size=2><b>Household Income( $ )</b></font>'))%>%
   addControl(html = "<img src='https://raw.githubusercontent.com/jz1584/Map/master/las%20vegas.JPG' style='width:337px;height:150px;'>",
              position = 'bottomleft')
-# labFormat = labelFormat('<b>',
-#                         suffix=c(' (Not affordable)',' (Less affordable)',' (Probably Affordable)',
-#                                  ' (More affordable)',' (Most affordable)')))
 
 
 
@@ -172,21 +190,22 @@ addPolygons(data=vegas,fillOpacity = 0.5,stroke=FALSE,group = '<font color="#cc0
 
 
 
+LV<-LasVegas
+
+for(i in unique(netdist$ID)){
+  LV<- addPolylines(LV, data = netdist[netdist$ID == i,],
+                    lat = ~lat, lng = ~lon,group ='<font color="#cc0000" size=4><u>Wynn Hotel</u></font> <b>(Red-Pulse Markers) with driving distance to call centers</b>',
+                    weight=2,color='blue',opacity=1)
+}
 
 
+LV%>%addPopups(lng=callCenters$lon,lat=callCenters$lat,
+               popup =paste('<font color="#cc0000"size=2><b>',callCenters$Wynn.distance.Mins,' mins</b></u></font>'),
+               options = popupOptions(closeButton = TRUE),
+               group = '<font color="#cc0000" size=4><u>Wynn Hotel</u></font> <b>(Red-Pulse Markers) with driving distance to call centers</b>'
+)
 
-a<-route(', NY 10007','Coney Island',structure = 'route',output = 'simple',mode='driving')
-library(geosphere)
-library(leaflet.extras)
 
-a<-geocode('City Hall Park, New York')
-b<-geocode('Coney Island')
-c<-geocode('brooklyn')
-leaflet()%>%addTiles()%>%
-  addGeodesicPolylines(lng=c(a$lon,b$lon),lat=c(a$lat,b$lat),steps=20000,popup = ('hello'),
-                       popupOptions =popupOptions(keepInView = TRUE) )%>%
-  addGeodesicPolylines(lng=c(a$lon,c$lon),lat=c(a$lat,c$lat),steps=10,popup = ('world'),
-                       popupOptions =popupOptions(keepInView = TRUE) )
 
 
 
